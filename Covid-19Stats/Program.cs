20fbeca12ea74d0cb8cb920f3499e8681a71ed41
Abdul-khaaliq Dollie <55828788+Abdulkhaaliq@users.noreply.github.com>
2020-03-25 21:15:56 +0200

    Initial

---
+++ Covid-19Stats/Program.cs
@@ -0,0 +1,23 @@
+﻿using CovidNineteenStatsLibrary;
+using System;
+using System.Threading.Tasks;
+
+namespace Covid_19Stats
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            StatGenerator statGenerator = new StatGenerator();
+
+            InformationHere informationHere = await StatGenerator.GetStatAsync();
+
+            Console.WriteLine(informationHere.total_cases);
+            
+            //Console.WriteLine($"{statGenerator.ToString()}");
+
+
+        }
+    }
+}
