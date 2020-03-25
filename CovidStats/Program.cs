9fafee62d2c76eb6ca60e94a0d4713bde3d68d86
Abdul-khaaliq Dollie <55828788+Abdulkhaaliq@users.noreply.github.com>
2020-03-25 22:20:13 +0200

    Working Console Api Caller

---
+++ CovidStats/Program.cs
@@ -0,0 +1,19 @@
+﻿using StatsLibrary;
+using System;
+using System.Threading.Tasks;
+
+namespace CovidStats
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StatsModel weatherBoy = new StatsModel();
+            Stats weatherInfo = await StatsGenerator.GetWeatherAsync();
+
+        
+            Console.Write("Active cases: " + weatherInfo.active);
+        }
+    }
+}
